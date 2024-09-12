3a1685ecd86a4cc2c8168d529d722af96e5df774
Almaz <almaz.kzn2005@yandex.ru>
2024-09-12 22:34:12 +0300

    fix: prevent instantiation of GameSettings utility class

--- src/main/java/backend/academy/settings/GameSettings.java
+++ src/main/java/backend/academy/settings/GameSettings.java
@@ -3,4 +3,6 @@ package backend.academy.settings;
 public class GameSettings {
     public static final int MAX_ATTEMPTS_COUNT = 10;
     public static final int MAX_WORD_LENGTH = 15;
+
+    private GameSettings() {}
 }
