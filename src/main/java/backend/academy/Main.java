05f151caa115d2db4c9fdf739561fc9cbd0e780e
Almaz <almaz.kzn2005@yandex.ru>
2024-09-17 21:26:41 +0300

    feat: add start logic to Main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,23 @@
 package backend.academy;
 
+import java.io.IOException;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
-    public static void main(String[] args) {
+    private static Game game;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        start();
+    }
+
+    private void init() {
+        game = new Game();
+    }
+
+    private void start() throws IOException {
+        game.initGame();
+        game.startGame();
     }
 }
